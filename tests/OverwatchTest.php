129aa094c1f1b531fbdde16dbead3196a4a48dc1
Ben <ben@modernmcguire.com>
2023-05-01 22:54:34 -0700

    ~ Rework tests to test package api instead of underlying logic

--- tests/OverwatchTest.php
+++ tests/OverwatchTest.php
@@ -1,39 +1,54 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Encryption\Encrypter;
-use Modernmcguire\Overwatch\Metrics\LaravelVersion;
-use Modernmcguire\Overwatch\Metrics\PhpVersion;
 use Modernmcguire\Overwatch\Overwatch;
+use Illuminate\Support\Facades\Artisan;
+use Modernmcguire\Overwatch\Metrics\PhpVersion;
+use Modernmcguire\Overwatch\Metrics\LaravelVersion;
+
+it('can run metrics', function () {
+    config([
+        'overwatch.metrics' => [
+            PhpVersion::class,
+            LaravelVersion::class,
+        ],
+    ]);
+
+    $data = Overwatch::run();
+
+    expect($data)->toHaveKey('laravel_version');
+    expect($data['laravel_version'])->toBe(app()->version());
+
+    expect($data)->toHaveKey('php_version');
+    expect($data['php_version'])->toBe(phpversion());
+});
+
+it('can run metrics locally', function(){
+    config([
+        'overwatch.metrics' => [
+            PhpVersion::class,
+            LaravelVersion::class,
+        ],
+    ]);
+
+    $this->artisan('overwatch:metrics')->assertExitCode(0);
+});
+
 
 it('can check signature', function () {
 
-    $secret = 'base64:'.base64_encode(Encrypter::generateKey(config('app.cipher')));
+    // capture output of artisan call
+    Artisan::call('overwatch:generate');
+    $output = Artisan::output();
+
+    $secret = Str::of($output)->after(': ')->trim()->toString();
     config(['overwatch.secret' => $secret]);
 
-    $overwatch = new Overwatch();
     $payload = json_encode(['timestamp' => now('UTC')->toDateTimeString()]);
     $convertedKey = base64_decode(substr(config('overwatch.secret'), strlen('base64:')));
     $encrypter = new Encrypter($convertedKey, config('app.cipher'));
     $encryptedPayload = $encrypter->encrypt($payload);
 
-    $overwatch->checkSignature($encryptedPayload);
-
-    // checkSignature aborts if it fails so it will never reach this point on failure.
-    expect(true)->toBeTrue();
-});
-
-it('can get laravel version', function () {
-    $controller = new LaravelVersion();
-
-    $response = $controller->handle();
-
-    expect($response)->toBe(app()->version());
-});
-
-it('can get php version', function () {
-    $controller = new PhpVersion();
-
-    $response = $controller->handle();
-
-    expect($response)->toBe(phpversion());
+    $this->postJson(route('overwatch'), ['payload' => $encryptedPayload])->assertStatus(200);
 });
